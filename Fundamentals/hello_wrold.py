ac74565c2fb227aba033794d16a89267092444a4
Mohamed Amin Amri <amrim893@gmail.com>
2023-05-15 13:57:44 +0200

    hello world

---
+++ Fundamentals/hello_wrold.py
@@ -0,0 +1,9 @@
+#greeting = "hello world"
+#name = "Mohamed amin amri"
+#print(greeting +name)
+#number = 33
+#print(greeting,number)
+
+food_one = "Mloukhiya"
+food_two = "paghetti"
+print(food_one,food_two)
