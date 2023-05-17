78b13c3557f9844a78fb8be3a4b3ef17661987c6
Mohamed Amin Amri <amrim893@gmail.com>
2023-05-17 02:05:15 +0200

    for loop

---
+++ Fundamentals/forloop.py
@@ -0,0 +1,23 @@
+for i in range(151):
+    print(i)
+
+for i in range(5, 1001, 5):
+    print(i)
+
+for i in range(1,101,1):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 ==0:
+        print("coding")
+    else:
+        print(i)
+
+total_sum = 0
+
+for i in range(1, 500001, 2):
+    total_sum += i
+
+print(total_sum)
+
+for i in range(2018, 0, -4):
+    print(i)
