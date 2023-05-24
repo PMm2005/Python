fc732210f570c3dfcae173c038a7697d160c3c02
Mohamed Amin Amri <amrim893@gmail.com>
2023-05-24 23:36:56 +0200

    Playground

---
+++ flask/Playground/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/play')
+def index2():
+    return render_template("index2.html")
+
+@app.route('/play/<string:x>/<string:color>')
+def index3(x,color):
+    count = int(x)
+    return render_template("index3.html", count=count, color=color)
+
+
+if __name__=="__main__":
+    app.run(debug=True)  
+
