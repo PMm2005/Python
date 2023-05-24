94a8933d166808d9cd64c1bf83097e9cbdc8157c
Mohamed Amin Amri <amrim893@gmail.com>
2023-05-25 00:46:47 +0200

    playground

--- flask/Playground/server.py
+++ flask/Playground/server.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index2():
     return render_template("index2.html")
 
+@app.route('/play/<string:x>')
+def boxes(x):
+    count = int(x)
+    return render_template("index.html", count=count)
+
 @app.route('/play/<string:x>/<string:color>')
 def index3(x,color):
     count = int(x)
